fedde5c2a387c148d22f5c13c09e03e24dde0363
Ryan McGinty <rpmcginty@users.noreply.github.com>
2023-09-29 16:06:56 -0700

    remove unnecessry constants

--- src/aibs_informatics_aws_utils/constants/s3.py
+++ src/aibs_informatics_aws_utils/constants/s3.py
@@ -1,20 +1,6 @@
-from enum import Enum
-
-
-class S3BucketName(str, Enum):
-    ASSETS = "assets"
-    COST_REPORTING = "cost-reporting"
-    FILE_STORE = "file-store"
-    COST_INVENTORY = "cost-inventory"
-    LANDING = "landing"
-    STAGING = "staging"
-
-
-S3_LANDING_BUCKET = S3BucketName.LANDING.value
-S3_ASSETS_BUCKET = S3BucketName.ASSETS.value
-S3_FILE_STORE_BUCKET = S3BucketName.FILE_STORE.value
-S3_COST_REPORTING_BUCKET = S3BucketName.COST_REPORTING.value
-S3_STAGING_BUCKET = S3BucketName.STAGING.value
+S3_LANDING_BUCKET = "landing"
+S3_ASSETS_BUCKET = "assets"
+S3_FILE_STORE_BUCKET = "file-store"
 
 
 S3_SOURCE_PATH_VAR = "S3_SOURCE_PATH"
