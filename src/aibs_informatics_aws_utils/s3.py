e3718a0b82fd4bc61e549d253447d4fc7b5ca6c0
Nicholas Mei <nicholas.mei@outlook.com>
2024-08-28 11:08:10 -0700

    Port retry settings from OCS versions of s3 util functions
    This commit ports the retry settings for the OCS versions of the
    following functions:
    - `upload_file`
    - `copy_s3_object`
    - `download_s3_object`
    
    Settings are from:
    https://github.com/AllenInstitute/genomics-cloud-services/pull/805
    https://github.com/AllenInstitute/genomics-cloud-services/pull/796/commits/1ff009c5406f301967c78ea13bcd3047ed392615
    
    Rationale: The retry settings from the OCS versions have been
    "battle tested" and can handle many more transfers than the
    vanilla retry settings

--- src/aibs_informatics_aws_utils/s3.py
+++ src/aibs_informatics_aws_utils/s3.py
@@ -251,7 +251,11 @@ def download_s3_object_prefix(
         )
 
 
-@retry((ConnectionClosedError, EndpointConnectionError, ResponseStreamingError))
+@retry(
+    (ConnectionClosedError, EndpointConnectionError, ResponseStreamingError, ClientError),
+    tries=10,
+    backoff=2.0,
+)
 def download_s3_object(
     s3_path: S3URI,
     local_path: Path,
@@ -373,7 +377,7 @@ def upload_folder(
     logger.info(f"Uploaded {len(local_paths)} files to: {s3_path}")
 
 
-@retry(ResponseStreamingError)
+@retry(ResponseStreamingError, tries=10, backoff=2.0)
 def upload_file(
     local_path: Union[str, Path],
     s3_path: S3URI,
@@ -751,7 +755,7 @@ client_error_code_check__SlowDown: Callable[[Exception], bool] = lambda ex: isin
 ) and client_error_code_check(ex, "SlowDown")
 
 
-@retry(ClientError, [client_error_code_check__SlowDown])
+@retry(ClientError, [client_error_code_check__SlowDown], tries=10, backoff=2.0)
 def copy_s3_object(
     source_path: S3URI,
     destination_path: S3URI,
